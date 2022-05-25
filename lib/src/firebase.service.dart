f733fdb8e566d0295295f47e222d1c743e9c8aeb
Charlou Aredidon <charlou.aredidon@beaconforce.io>
2022-05-25 22:28:25 +0800

    setup notification logic

--- lib/src/firebase.service.dart
+++ lib/src/firebase.service.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:token_signal_app/firebase_options.dart';
 
 Future<void> configFirebase() async {
@@ -8,4 +11,62 @@ Future<void> configFirebase() async {
   );
 
   await FirebaseMessaging.instance.subscribeToTopic('all');
+}
+
+Completer firebaseInitializer = Completer();
+Future<void> waitForFirebaseInit() async {
+  await firebaseInitializer.future;
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+}
+
+Future<void> initFirebaseNotification() async {
+  try {
+    await Firebase.initializeApp();
+
+    // Set the background messaging handler early on, as a named top-level function
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      if (kDebugMode) {
+        print('User granted permission');
+      }
+    } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+      if (kDebugMode) {
+        print('User granted provisional permission');
+      }
+    } else {
+      if (kDebugMode) {
+        print('User declined or has not accepted permission');
+      }
+    }
+
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    if (!firebaseInitializer.isCompleted) firebaseInitializer.complete();
+  } catch (e) {
+    if (kDebugMode) {
+      print(e);
+    }
+  }
 }
---
+++ lib/src/notification.local.dart
@@ -0,0 +1,40 @@
+import 'dart:math';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+const MacOSInitializationSettings initializationSettingsMacOS = MacOSInitializationSettings();
+const InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid, macOS: initializationSettingsMacOS);
+
+Future<void> initLocalNotification() async {
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: onSelectNotification);
+}
+
+Future<void> showLocalNotification(RemoteMessage? message) async {
+  if (message != null) {
+    final random = Random();
+    int next(int min, int max) => min + random.nextInt(max - min);
+
+    const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'high_importance_channel',
+      'Quantz Notifications',
+      channelDescription: 'Notifications for episode releases and news updates.',
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: true,
+    );
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      next(10000000, 99999999),
+      message.notification?.title,
+      message.notification?.body,
+      platformChannelSpecifics,
+      payload: '{}',
+    );
+  }
+}
+
+Future onSelectNotification(String? event) async {}
